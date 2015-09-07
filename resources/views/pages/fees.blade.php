e32f8af992a3d9a87c165544bebd709971e76f7d
Jenny Swift <cheezyspaghetti@gmail.com>
2015-09-08 09:24:59 +1000

    Fees page

--- resources/views/pages/fees.blade.php
+++ resources/views/pages/fees.blade.php
@@ -5,7 +5,15 @@
 @section('page-content')
 
 
-    <div>
+    <div id="fees">
+
+        <h1>Fees</h1>
+
+        <p>Initial Consultation (90 minutes) - $130</p>
+        <p>Long Consultation (15-30 minutes) - $70</p>
+        <p>Standard Consultation (up to 15 minutes) - $55</p>
+        <p>Credit/debit cards accepted.</p>
+        <p>Chiropractic is usually included in 'extras' if you have private health insurance. If you would like to know the details of your cover please contact your insurance company.</p>
 
     </div>
 
