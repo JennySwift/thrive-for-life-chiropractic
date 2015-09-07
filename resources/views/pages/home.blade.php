72bb45d8ccdb5f0a3c0f4d80c62f7af7e1c84e7b
Jenny Swift <cheezyspaghetti@gmail.com>
2015-09-07 16:17:30 +1000

    Home page and cleanup

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -9,6 +9,11 @@
 
     @include('templates.logo')
 
+    <div id="text">
+        <p>Welcome! I'm Dr Peter Swift, chiropractor and owner of Thrive for Life Chiropractic.</p>
+        <p>My aim is to help you love life (and achieve your goals/dreams), by naturally supporting your body's efforts to heal, be strong, feel great, and conquer the challenges each day brings. If you want to get well and stay well - not just survive, but thrive - I'm here for you.</p>
+    </div>
+
 </div>
 
 @stop
