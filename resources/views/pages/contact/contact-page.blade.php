b423d5f7724423ed4210dc67a3ff9db6714fb301
Jenny Swift <cheezyspaghetti@gmail.com>
2016-01-21 21:58:46 +1100

    Contact page

--- resources/views/pages/contact/contact-page.blade.php
+++ resources/views/pages/contact/contact-page.blade.php
@@ -2,6 +2,22 @@
 
 @section('page-content')
 
-    <div id="contact"></div>
+    <div id="contact">
+
+        <div class="text-container">
+            <div class="text">
+                <h1>Contact</h1>
+                <ul>
+                    <li>Mon 9am-6pm</li>
+                    <li>Tue 10am-12pm</li>
+                    <li>Wed closed</li>
+                    <li>Thu 9am-5pm</li>
+                    <li>Fri 10:30am-6pm</li>
+                    <li>Sat 9am-12pm</li>
+                </ul>
+            </div>
+        </div>
+
+    </div>
 
 @stop
