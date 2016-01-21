c7950c30560dd0eb64ba0dd20c055817c59cf009
Jenny Swift <cheezyspaghetti@gmail.com>
2016-01-22 10:37:49 +1100

    Add missing text to home page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -16,7 +16,7 @@
                 <h2>Welcome!</h2>
                 <p>I'm Dr Peter Swift, chiropractor and owner of <i>Thrive for Life Chiropractic.</i></p>
                 <p>I’m here to help you love life - to naturally support your body's own efforts to heal, be strong, feel great, and conquer the challenges each day brings.</p>
-                <p>Text missing here in Pages.</p>
+                <p>If you want to get well and stay well - not just survive, but thrive - I'm here for you.</p>
             </div>
             <div class="img-container">
                 <img src="/img/Welcome/welcome.png">
