6c413b02866e8e88663658df77d96a0e352f9197
Jenny Swift <cheezyspaghetti@gmail.com>
2016-01-22 19:25:32 +1100

    Contact page

--- resources/views/pages/contact/contact-page.blade.php
+++ resources/views/pages/contact/contact-page.blade.php
@@ -7,14 +7,28 @@
         <div class="text-container">
             <div class="text">
                 <h1>Contact</h1>
-                <ul>
-                    <li>Mon 9am-6pm</li>
-                    <li>Tue 10am-12pm</li>
-                    <li>Wed closed</li>
-                    <li>Thu 9am-5pm</li>
-                    <li>Fri 10:30am-6pm</li>
-                    <li>Sat 9am-12pm</li>
-                </ul>
+                <div class="flex">
+
+                    <ul>
+                        <li>
+                            <a href="mailto:peter.c.swift@gmail.com">peter.c.swift<i class="fa fa-at"></i>gmail.com</a>
+                        </li>
+                        <li>
+                            <div>40 Vine St</div>
+                            <div>Hurstville NSW</div>
+                        </li>
+                    </ul>
+
+                    <ul>
+                        <li>Mon 9am-6pm</li>
+                        <li>Tue 10am-12pm</li>
+                        <li>Wed closed</li>
+                        <li>Thu 9am-5pm</li>
+                        <li>Fri 10:30am-6pm</li>
+                        <li>Sat 9am-12pm</li>
+                    </ul>
+                </div>
+
             </div>
         </div>
 
