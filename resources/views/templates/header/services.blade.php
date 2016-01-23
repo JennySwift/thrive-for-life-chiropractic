0ef3e0d51dc67408389554092115dcc8afdaebb3
Jenny Swift <cheezyspaghetti@gmail.com>
2016-01-23 13:49:54 +1100

    Change order of services in header dropdown

--- resources/views/templates/header/services.blade.php
+++ resources/views/templates/header/services.blade.php
@@ -4,11 +4,11 @@
         <span class="caret"></span>
     </a>
     <ul class="dropdown-menu" role="menu">
-        <li><a href="/services/ak">AK</a></li>
         <li><a href="/services/chiropractic">Chiropractic</a></li>
-        <li><a href="/services/government">Government</a></li>
-        <li><a href="/services/hra">HRA</a></li>
-        <li><a href="/services/lifestyle">lifestyle</a></li>
+        <li><a href="/services/ak">AK</a></li>
         <li><a href="/services/rnr">RNR</a></li>
+        <li><a href="/services/lifestyle">lifestyle</a></li>
+        <li><a href="/services/hra">HRA</a></li>
+        <li><a href="/services/government">Government</a></li>
     </ul>
 </li>
