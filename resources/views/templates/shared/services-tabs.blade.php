9157777c5f5e0a34bafd0e27aa3db7f2566bd4e2
Jenny Swift <cheezyspaghetti@gmail.com>
2016-01-25 11:30:44 +1100

    Don't use abbreviations for services tabs

--- resources/views/templates/shared/services-tabs.blade.php
+++ resources/views/templates/shared/services-tabs.blade.php
@@ -1,8 +1,8 @@
 <ul class="tabs">
     <li class="tab"><a v-link="{path: '/services/chiropractic'}" v-bind:class="{'current-page': path === '/services/chiropractic'}" href="#">Chiropractic</a></li>
-    <li class="tab"><a v-link="{path: '/services/ak'}" href="#" v-bind:class="{'current-page': path === '/services/ak'}">AK</a></li>
-    <li class="tab"><a v-link="{path: '/services/rnr'}" href="#" v-bind:class="{'current-page': path === '/services/rnr'}">RNR</a></li>
-    <li class="tab"><a v-link="{path: '/services/lifestyle'}" href="#" v-bind:class="{'current-page': path === '/services/lifestyle'}">lifestyle</a></li>
-    <li class="tab"><a v-link="{path: '/services/hra'}" href="#" v-bind:class="{'current-page': path === '/services/hra'}">HRA</a></li>
+    <li class="tab"><a v-link="{path: '/services/ak'}" href="#" v-bind:class="{'current-page': path === '/services/ak'}">Applied Kinesiology</a></li>
+    <li class="tab"><a v-link="{path: '/services/rnr'}" href="#" v-bind:class="{'current-page': path === '/services/rnr'}">Retained Neonatal Reflexes</a></li>
+    <li class="tab"><a v-link="{path: '/services/lifestyle'}" href="#" v-bind:class="{'current-page': path === '/services/lifestyle'}">Lifestyle</a></li>
+    <li class="tab"><a v-link="{path: '/services/hra'}" href="#" v-bind:class="{'current-page': path === '/services/hra'}">Health Risk Assessment</a></li>
     <li class="tab"><a v-link="{path: '/services/government'}" href="#" v-bind:class="{'current-page': path === '/services/government'}">Government</a></li>
 </ul>
