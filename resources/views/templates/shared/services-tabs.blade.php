304f48e9c0a37bbb0ceecf7976848b74e8f27dcf
Jenny Swift <cheezyspaghetti@gmail.com>
2016-01-25 22:13:28 +1100

    Tab styling

--- resources/views/templates/shared/services-tabs.blade.php
+++ resources/views/templates/shared/services-tabs.blade.php
@@ -1,60 +1,91 @@
 <ul class="tabs">
-    <li class="tab">
-        <a
-            {{--v-link="{path: '/services/chiropractic'}"--}}
-            v-on:click="path = '/services/chiropractic'"
-            href="#"
-            v-bind:class="{'current-page': path === '/services/chiropractic'}"
-        >
-            Chiropractic
-        </a>
+
+    <li
+        v-bind:class="{'current-tab': path === '/services/chiropractic', 'next-to-current-tab': path === '/services/ak'}"
+        class="tab"
+    >
+        <div class="tab-inner">
+            <a
+                {{--v-link="{path: '/services/chiropractic'}"--}}
+                v-on:click="path = '/services/chiropractic'"
+                href="#"
+            >
+                Chiropractic
+            </a>
+        </div>
     </li>
-    <li class="tab">
-        <a
-            {{--v-link="{path: '/services/ak'}"--}}
-            v-on:click="path = '/services/ak'"
-            href="#"
-            v-bind:class="{'current-page': path === '/services/ak'}"
-        >
-            Applied Kinesiology
-        </a>
+
+    <li
+        v-bind:class="{'current-tab': path === '/services/ak', 'next-to-current-tab': path === '/services/rnr'}"
+        class="tab"
+    >
+        <div class="tab-inner">
+            <a
+                    {{--v-link="{path: '/services/ak'}"--}}
+                    v-on:click="path = '/services/ak'"
+                    href="#"
+            >
+                Applied Kinesiology
+            </a>
+        </div>
     </li>
-    <li class="tab">
-        <a
-            {{--v-link="{path: '/services/rnr'}"--}}
-            v-on:click="path = '/services/rnr'"
-            href="#"
-            v-bind:class="{'current-page': path === '/services/rnr'}"
-        >
-            Retained Neonatal Reflexes
-        </a>
+
+    <li
+        v-bind:class="{'current-tab': path === '/services/rnr', 'next-to-current-tab': path === '/services/lifestyle'}"
+        class="tab"
+    >
+        <div class="tab-inner">
+            <a
+                    {{--v-link="{path: '/services/rnr'}"--}}
+                    v-on:click="path = '/services/rnr'"
+                    href="#"
+            >
+                Retained Neonatal Reflexes
+            </a>
+        </div>
     </li>
-    <li class="tab">
-        <a
-            {{--v-link="{path: '/services/lifestyle'}"--}}
-            v-on:click="path = '/services/lifestyle'"
-            href="#"
-            v-bind:class="{'current-page': path === '/services/lifestyle'}"
-        >
-            Lifestyle</a></li>
-    <li class="tab">
-        <a
-            {{--v-link="{path: '/services/hra'}"--}}
-            v-on:click="path = '/services/hra'"
-            href="#"
-            v-bind:class="{'current-page': path === '/services/hra'}"
-        >
-            Health Risk Assessment
-        </a>
+
+    <li
+        v-bind:class="{'current-tab': path === '/services/lifestyle', 'next-to-current-tab': path === '/services/hra'}"
+        class="tab"
+    >
+        <div class="tab-inner">
+            <a
+                    {{--v-link="{path: '/services/lifestyle'}"--}}
+                    v-on:click="path = '/services/lifestyle'"
+                    href="#"
+            >
+                Lifestyle
+            </a>
+        </div>
     </li>
-    <li class="tab">
-        <a
-            {{--v-link="{path: '/services/government'}"--}}
-            v-on:click="path = '/services/government'"
-            href="#"
-            v-bind:class="{'current-page': path === '/services/government'}"
-        >
-            Government
-        </a>
+
+
+    <li
+        v-bind:class="{'current-tab': path === '/services/hra', 'next-to-current-tab': path === '/services/government'}"
+        class="tab"
+    >
+        <div class="tab-inner">
+            <a
+                    {{--v-link="{path: '/services/hra'}"--}}
+                    v-on:click="path = '/services/hra'"
+                    href="#"
+            >
+                Health Risk Assessment
+            </a>
+        </div>
     </li>
+
+    <li v-bind:class="{'current-tab': path === '/services/government'}" class="tab">
+        <div class="tab-inner">
+            <a
+                    {{--v-link="{path: '/services/government'}"--}}
+                    v-on:click="path = '/services/government'"
+                    href="#"
+            >
+                Government
+            </a>
+        </div>
+    </li>
+
 </ul>
