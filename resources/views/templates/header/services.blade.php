4b901926f798858f099a4de30be3ae578bde11ff
Jenny Swift <cheezyspaghetti@gmail.com>
2016-02-11 10:45:34 +1100

    Adjust underline for services link in header

--- resources/views/templates/header/services.blade.php
+++ resources/views/templates/header/services.blade.php
@@ -1,11 +1,18 @@
-<li
-    v-bind:class="{'current-page': path.indexOf('/services') !== -1}">
+<li class="services">
+    <a
+        href="#"
+        v-bind:class="{'current-page': path.indexOf('/services') !== -1}"
+        {{--v-link="{path: '/services/chiropractic'}"--}}
+        v-on:click="toggleServicesTabs()"
+    >
+        Services
+    </a>
+
     <a
         href="#"
         {{--v-link="{path: '/services/chiropractic'}"--}}
         v-on:click="toggleServicesTabs()"
     >
-        Services
         <span v-show="showServicesTabs" class="fa fa-chevron-up"></span>
         <span v-show="!showServicesTabs" class="fa fa-chevron-down"></span>
     </a>
