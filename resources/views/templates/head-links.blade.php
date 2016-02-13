99dd8d0d042c334ced8d6efaab2a1013e6798ccc
Jenny Swift <cheezyspaghetti@gmail.com>
2016-02-13 19:47:59 +1100

    Use Google maps again

--- resources/views/templates/head-links.blade.php
+++ resources/views/templates/head-links.blade.php
@@ -1,3 +1,3 @@
-{{--<link href='https://fonts.googleapis.com/css?family=Josefin+Sans:400,700,700italic|Annie+Use+Your+Telescope|Architects+Daughter' rel='stylesheet' type='text/css'>--}}
+<link href='https://fonts.googleapis.com/css?family=Josefin+Sans:400,700,700italic|Annie+Use+Your+Telescope|Architects+Daughter' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" href="/css/plugins.css">
 <link rel="stylesheet" href="{{ elixir("css/app.css") }}">
