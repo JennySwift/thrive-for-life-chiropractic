171960810a6893553b31deb8511346a1f6296fb5
Jenny Swift <cheezyspaghetti@gmail.com>
2016-03-11 15:57:45 +1100

    Change content on government page

--- resources/views/pages/services/government-page-component.blade.php
+++ resources/views/pages/services/government-page-component.blade.php
@@ -17,8 +17,7 @@
                                     calendar year. A chronic medical condition is one that has been (or is likely to be) present for six
                                     months or longer.</p>
                                 <p>Eligibility requires a referral from your GP.</p>
-                                <p>For more information visit
-                                    www.health.gov.au/internet/main/publishing.nsf/Content/health-medicare-allied-health-brochure.htm</p>
+                                <p>If you would like more information, please click <a href="www.health.gov.au/internet/main/publishing.nsf/Content/ health-medicare-allied-health-brochure.htm">here</a>.</p>
                             </div>
                         </div>
                     </accordion-item>
@@ -33,8 +32,7 @@
                                     holders. For White Card holders, DVA will fund those services required because of their accepted
                                     war-caused or service-related condition(s).</p>
                                 <p>Eligibility requires a referral.</p>
-                                <p>For more information visit
-                                    factsheets.dva.gov.au/factsheets/documents/HSV13%20Chiropractic%20Services.htm</p>
+                                <p>If you would like more information, please click <a href="factsheets.dva.gov.au/factsheets/documents/ HSV13%20Chiropractic%20Services.htm">here</a>.</p>
                             </div>
                         </div>
                     </accordion-item>
@@ -48,7 +46,8 @@
                                     expenses for specialist consultations. This amount is a family allocation which can be transferred
                                     between family members (for example, a family of 3 dependants will be allocated $1,200 per financial
                                     year).</p>
-                                <p>For more information visit www.defence.gov.au/Health/SHC/DependantHealthcare/default.asp</p>
+                                <p>If you would like more information, please click <a href="www.defence.gov.au/Health/SHC/DependantHealthcare/
+                                    default.asp">here</a>.</p>
                             </div>
                         </div>
                     </accordion-item>
