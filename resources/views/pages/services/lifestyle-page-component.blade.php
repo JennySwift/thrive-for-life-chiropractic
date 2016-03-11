10e3757b4a6240410758d18ba4d85b7ef43b1885
Jenny Swift <cheezyspaghetti@gmail.com>
2016-03-11 15:51:03 +1100

    Change content on lifestyle page

--- resources/views/pages/services/lifestyle-page-component.blade.php
+++ resources/views/pages/services/lifestyle-page-component.blade.php
@@ -4,8 +4,8 @@
         <div class="cover"></div>
         <div class="text">
             <h1>Health-Promoting Lifestyle Coaching</h1>
-            <p>The biggest determining factor in whether you enjoy good health or suffer from chronic illness, and also how much chiropractic treatment you will need, is lifestyle. Nothing has a greater impact on your overall health, well-being, performance and vitality than your day-to-day habits and activities - how you eat, move, think, sleep, etc.</p>
-            <p>My aim is to help you understand the cause of your problems and empower you to be part of the solution. My health-promoting lifestyle advice enables you to be proactive in maintaining the benefits of treatment and preventing further problems from occurring in the future. It beautifully compliments and maximises the value of chiropractic care and is a fundamental part of the holistic service I offer.</p>
+            <p>The biggest determining factor in whether you enjoy good health or suffer from chronic illness — and also how much chiropractic treatment you will need — is your lifestyle. Nothing has a greater impact on your overall health, well-being, performance and vitality than your day-to-day habits and activities — how you eat, move, think, sleep, etc.</p>
+            <p>My aim is to help you understand the cause of your problems and empower you to be part of the solution. My health-promoting lifestyle coaching enables you to be proactive in maintaining the benefits of treatment and preventing further problems from occurring in the future. It wonderfully compliments and maximises the value of chiropractic care and is a fundamental part of the holistic service I offer.</p>
         </div>
     </div>
 
