de16a70f4a8d33a4fcb9f4ae382556382160bbe4
Jenny Swift <cheezyspaghetti@gmail.com>
2016-03-11 13:17:48 +1100

    Work on contact details transition

--- resources/assets/js/transitions/ContactDetailsTransition.js
+++ resources/assets/js/transitions/ContactDetailsTransition.js
@@ -1,31 +1,31 @@
-Vue.transition('contact-details', {
-    css: false,
-    enter: function (el, done) {
-        var that = this;
-
-        var clone = $(el).clone()
-            .css({height: 'auto', width: 'auto'})
-            .appendTo($(el).closest('.text'));
-
-        var height = clone.height();
-        var width = clone.width();
-        clone.remove();
-
-        $(el).animate({
-            height: height,
-            width: width
-        }, 800, done);
-    },
-    enterCancelled: function (el) {
-        $(el).stop()
-    },
-    leave: function (el, done) {
-        $(el).animate({
-            height: 0,
-            width: 0,
-        }, 800, done)
-    },
-    leaveCancelled: function (el) {
-        $(el).stop()
-    },
-});
+//Vue.transition('contact-details', {
+//    css: false,
+//    enter: function (el, done) {
+//        var that = this;
+//
+//        var clone = $(el).clone()
+//            .css({height: 'auto', width: 'auto'})
+//            .appendTo($(el).closest('.text'));
+//
+//        var height = clone.height();
+//        var width = clone.width();
+//        clone.remove();
+//
+//        $(el).animate({
+//            height: height,
+//            width: width
+//        }, 800, done);
+//    },
+//    enterCancelled: function (el) {
+//        $(el).stop()
+//    },
+//    leave: function (el, done) {
+//        $(el).animate({
+//            height: 0,
+//            width: 0,
+//        }, 800, done)
+//    },
+//    leaveCancelled: function (el) {
+//        $(el).stop()
+//    },
+//});
