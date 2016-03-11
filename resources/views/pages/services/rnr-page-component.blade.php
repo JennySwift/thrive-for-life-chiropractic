fc17c956f0bcefeab3744c84fcdab86f962b86e2
Jenny Swift <cheezyspaghetti@gmail.com>
2016-03-11 15:48:33 +1100

    Change content on RNR page

--- resources/views/pages/services/rnr-page-component.blade.php
+++ resources/views/pages/services/rnr-page-component.blade.php
@@ -4,19 +4,11 @@
         <div class="cover"></div>
         <div class="text scrollbar-container">
             <h1>Retained Neonatal Reflexes</h1>
-            <p>Hard-wired into the nervous system (specifically the brainstem) are a number of reflexes which help
-                children survive the birthing process and the early stages of life before they develop the ability to
-                make decisions. These are called 'primitive' or 'neonatal' reflexes.</p>
-            <p>As the nervous system matures, the neonatal reflexes are supposed to take a 'back seat' and allow the
-                higher processing or thinking areas of the brain to take control. In some cases, however, the neonatal
-                reflexes remain dominant (often due to significant stress such as birth trauma). This disharmony in the
-                nervous system can result in undesirable reactions in certain situations and can adversely affect
-                development, learning, coordination, and behaviour.</p>
-            <p>Specific structural corrections can assist the integration of Retained Neonatal Reflexes, thus restoring
-                harmony within the nervous system and allowing children to develop to their full potential.</p>
-            <p>Retained Neonatal Reflexes can also be present in adults. Correcting/integrating them may help resolve
-                chronic, recurring problems.</p>
-            <p>For more information visit www.retainedneonatalreflexes.com.au</p>
+            <p>Hard-wired into the nervous system (specifically the brainstem) are a number of reflexes which help children survive the birthing process and the early stages of life before they develop the ability to make decisions. These are called 'primitive' or 'neonatal' reflexes.</p>
+            <p>As the nervous system matures, the neonatal reflexes should ideally take a 'back seat' and allow the higher processing or thinking areas of the brain to take control. In some cases, however, the neonatal reflexes remain dominant (often due to significant stress such as birth trauma). This disharmony in the nervous system can result in problems such as developmental delays, learning difficulties, poor coordination and behavioural problems.</p>
+            <p>Specific structural corrections can assist the integration of Retained Neonatal Reflexes, thus restoring harmony within the nervous system and allowing children to develop to their full potential.</p>
+            <p>Retained Neonatal Reflexes can also be present in adults. Assisting their integration may help resolve chronic, recurring problems.</p>
+            <p>If you would like more information about each individual reflex and the symptoms commonly-associated with them, please click <a href="www.retainedneonatalreflexes.com.au/reflexes-explained/">here</a>.</p>
         </div>
     </div>
 
