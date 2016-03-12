8220e06275f57d4c3a182d0c7b13b7feb65d4f73
Jenny Swift <cheezyspaghetti@gmail.com>
2016-03-13 10:13:11 +1100

    Try to get media queries working for iPhone

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,7 +3,7 @@
 <html lang="en" class="">
 
 <head>
-    <meta charset="UTF-8">
+    <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Thrive For Life</title>
     @include('templates.head-links')
 </head>
