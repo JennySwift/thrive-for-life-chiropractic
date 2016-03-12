72b533307bce5d815c39feb03baf75d13281f5d7
Jenny Swift <cheezyspaghetti@gmail.com>
2016-03-12 16:50:05 +1100

    Remove login requirement

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -16,7 +16,7 @@ class PagesController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+//        $this->middleware('auth');
     }
 
     /**
