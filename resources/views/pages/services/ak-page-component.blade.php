587e6079d380f20a84bcf6e1eb66a1f1b2e57faf
Jenny Swift <cheezyspaghetti@gmail.com>
2016-03-13 12:27:17 +1100

    Make ak page better for iPhone

--- resources/views/pages/services/ak-page-component.blade.php
+++ resources/views/pages/services/ak-page-component.blade.php
@@ -2,7 +2,7 @@
 
     <div id="ak" class="service">
         <div class="cover"></div>
-        <div class="text">
+        <div class="text scrollbar-container">
             <h1>Applied Kinesiology</h1>
             <p>Applied Kinesiology (AK) is a non-invasive diagnostic method that uses manual muscle testing to evaluate the integrity of the nervous system. Given that the nervous system monitors and coordinates every aspect of the body, AK can be used to evaluate the whole body and is helpful in discovering the reasons for functional disturbances and also in finding a solution.</p>
         </div>
