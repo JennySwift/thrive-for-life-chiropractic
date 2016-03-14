5b5511165fddce8f56f2ff5582ee9a9bec9cf34d
Jenny Swift <cheezyspaghetti@gmail.com>
2016-03-15 10:36:55 +1100

    Change title

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,7 +4,7 @@
 
 <head>
     <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-    <title>Thrive For Life</title>
+    <title>Thrive for Life Chiropractic</title>
     @include('templates.head-links')
 </head>
 
