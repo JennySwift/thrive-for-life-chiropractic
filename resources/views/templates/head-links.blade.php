3daf5738ddbaf83d11103ba5bbf477cf76180c47
Jenny Swift <cheezyspaghetti@gmail.com>
2016-03-15 17:45:41 +1100

    Undo last commit

--- resources/views/templates/head-links.blade.php
+++ resources/views/templates/head-links.blade.php
@@ -1,4 +1,3 @@
 <link href='https://fonts.googleapis.com/css?family=Josefin+Sans:400,400italic,700,700italic|Annie+Use+Your+Telescope|Architects+Daughter' rel='stylesheet' type='text/css'>
-@import url(http://fonts.googleapis.com/earlyaccess/nanumpenscript.css);
 <link rel="stylesheet" href="/css/plugins.css">
 <link rel="stylesheet" href="{{ elixir("css/app.css") }}">
