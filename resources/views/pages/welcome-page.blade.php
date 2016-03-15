241003c3f9a60d7fa256deb29a35426abbb19b5a
Jenny Swift <cheezyspaghetti@gmail.com>
2016-03-16 10:47:16 +1100

    Small image for welcome page for smaller screens

--- resources/views/pages/welcome-page.blade.php
+++ resources/views/pages/welcome-page.blade.php
@@ -6,7 +6,11 @@
     <div class="flex">
         <div class="scrollbar-container">
             <div class="text scrollbar-content">
-                <h1>Welcome!</h1>
+                <h1 class="big-screens">Welcome!</h1>
+                <div class="small-screens">
+                    <h1>Welcome!</h1>
+                    <img src="/img/Welcome/welcome-img.jpg">
+                </div>
                 <p>I'm Dr Peter Swift, chiropractor and owner of <i>Thrive for Life Chiropractic.</i></p>
                 <p>I’m here to help you thrive — to naturally support your body's own efforts to heal, be strong, feel great, and conquer the challenges each day brings.</p>
                 <p>If you want to get well and stay well — not just survive, but thrive — I'm here for you.</p>
