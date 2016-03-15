ec961569533caa70a97375473f84f42851b5f674
Jenny Swift <cheezyspaghetti@gmail.com>
2016-03-15 22:15:31 +1100

    Play with css to try to fix iPhone scroll bug on welcome page

--- resources/views/pages/home-page-component.blade.php
+++ resources/views/pages/home-page-component.blade.php
@@ -2,50 +2,26 @@
 
     <div id="home-page-container">
         <div id="home">
-            <div class="text">
-                <img src="/img/Home/thrive-for-life-text.png">
-                {{--<div>thrive for life</div>--}}
-                {{--<div>CHIROPRACTIC</div>--}}
-            </div>
-            <a href="#welcome">
-                {{--<i class="fa fa-chevron-circle-down"></i>--}}
-                <span class="arrow fa fa-chevron-circle-down"></span>
-            </a>
+            {{--<div class="text">--}}
+                {{--<img src="/img/Home/thrive-for-life-text.png">--}}
+            {{--</div>--}}
+            {{--<a href="#welcome">--}}
+                {{--<span class="arrow fa fa-chevron-circle-down"></span>--}}
+            {{--</a>--}}
         </div>
 
-        <div id="welcome">
-            <div class="cover"></div>
-            <a href="#home">
-                {{--<i class="fa fa-chevron-circle-up"></i>--}}
-                <span class="arrow fa fa-chevron-circle-down"></span>
-            </a>
-            {{--<img class="background-img" src="/img/Home/spring-small-compressed.jpg">--}}
-            <div class="flex">
-                <div class="scrollbar-container">
-                    <div class="text scrollbar-content">
-                        <h1>Welcome!</h1>
-                        <p>I'm Dr Peter Swift, chiropractor and owner of <i>Thrive for Life Chiropractic.</i></p>
-                        <p>I’m here to help you thrive — to naturally support your body's own efforts to heal, be strong, feel great, and conquer the challenges each day brings.</p>
-                        <p>If you want to get well and stay well — not just survive, but thrive — I'm here for you.</p>
-                    </div>
-                </div>
-
-                <div class="img-container">
-                    <img src="/img/Welcome/welcome-img.jpg">
-                </div>
-            </div>
-        </div>
+        @include('pages.welcome-page')
 
         {{--This is so that Firefox doesn't load an image when the user switches page--}}
-        <div id="preloaded-images">
-            <img src="/img/About/about-quality5.jpg">
-            <img src="/img/Chiropractic/balance-compressed.jpg">
-            <img src="/img/AK/walk-small-compressed.jpg">
-            <img src="/img/RNR/child-small-compressed.jpg">
-            <img src="/img/Lifestyle/hiker-small-compressed.jpg">
-            <img src="/img/HRA/tape-small.jpg">
-            <img src="/img/Government/meadow-small-compressed.jpg">
-        </div>
+        {{--<div id="preloaded-images">--}}
+            {{--<img src="/img/About/about-quality5.jpg">--}}
+            {{--<img src="/img/Chiropractic/balance-compressed.jpg">--}}
+            {{--<img src="/img/AK/walk-small-compressed.jpg">--}}
+            {{--<img src="/img/RNR/child-small-compressed.jpg">--}}
+            {{--<img src="/img/Lifestyle/hiker-small-compressed.jpg">--}}
+            {{--<img src="/img/HRA/tape-small.jpg">--}}
+            {{--<img src="/img/Government/meadow-small-compressed.jpg">--}}
+        {{--</div>--}}
     </div>
 
 </script>
---
+++ resources/views/pages/welcome-page.blade.php
@@ -0,0 +1,22 @@
+<div id="welcome">
+    {{--<div class="cover"></div>--}}
+    {{--<a href="#home">--}}
+        {{--<i class="fa fa-chevron-circle-up"></i>--}}
+        {{--<span class="arrow fa fa-chevron-circle-down"></span>--}}
+    {{--</a>--}}
+    {{--<img class="background-img" src="/img/Home/spring-small-compressed.jpg">--}}
+    {{--<div class="flex">--}}
+        {{--<div class="scrollbar-container">--}}
+            {{--<div class="text scrollbar-content">--}}
+                {{--<h1>Welcome!</h1>--}}
+                {{--<p>I'm Dr Peter Swift, chiropractor and owner of <i>Thrive for Life Chiropractic.</i></p>--}}
+                {{--<p>I’m here to help you thrive — to naturally support your body's own efforts to heal, be strong, feel great, and conquer the challenges each day brings.</p>--}}
+                {{--<p>If you want to get well and stay well — not just survive, but thrive — I'm here for you.</p>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+
+        {{--<div class="img-container">--}}
+            {{--<img src="/img/Welcome/welcome-img.jpg">--}}
+        {{--</div>--}}
+    {{--</div>--}}
+</div>
