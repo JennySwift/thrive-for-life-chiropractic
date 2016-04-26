c9867dc59871095e732030d2785d0d15f2d575a4
Jenny Swift <cheezyspaghetti@gmail.com>
2016-04-26 11:41:10 +1000

    Add copyright to footer

--- resources/views/templates/shared/footer-component.blade.php
+++ resources/views/templates/shared/footer-component.blade.php
@@ -2,6 +2,10 @@
 
     <footer v-bind:class="{'contact-page-footer': path === '/contact'}">
         <li>
+            <div class="copyright"><i class="fa fa-copyright"></i> <span>2016 Thrive for Life Chiropractic</span></div>
+        </li>
+
+        <li>
             <a href="#" v-on:click="showPrivacyPolicyPopup()">Privacy Policy</a>
         </li>
     </footer>
