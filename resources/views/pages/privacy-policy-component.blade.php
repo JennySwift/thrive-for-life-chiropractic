d0d4010e7950437324340340111466682b00ee8d
Jenny Swift <cheezyspaghetti@gmail.com>
2016-04-26 12:58:17 +1000

    Bits and pieces

--- resources/views/pages/privacy-policy-component.blade.php
+++ resources/views/pages/privacy-policy-component.blade.php
@@ -7,19 +7,20 @@
         :redirect-to="redirectTo"
     >
         <div slot="content">
+
+            <i
+                    v-on:click="showPopup = false"
+                    class="fa fa-times"
+            >
+            </i>
+
             <h1>Privacy Policy</h1>
 
             <p>Just like I want you to feel safe and comfortable in my clinic, I want you to feel safe and comfortable on my website too. If you have any questions or concerns about my Privacy Policy, please feel free to
                 <a href="mailto:peter.c.swift@gmail.com">email me</a>.</p>
 
             <h2>Google Analytics</h2>
-            <p>Like millions of other websites, thriveforlifechiropractic.com.au uses Google Analytics to record <i>anonymous</i> information about how visitors are using my website. Some of this information includes:</p>
-            <ul>
-                <li>how many visitors the site receives</li>
-                <li>how long visitors spend on a page</li>
-                <li>what links are clicked</li>
-                <li>search terms that led to a visit</li>
-            </ul>
+            <p>Like millions of other websites, thriveforlifechiropractic.com.au uses Google Analytics to record <i>anonymous</i> information about how visitors are using the site. This includes information such as: how many visitors the site receives, how long visitors spend on a page, what links are clicked, search terms that led to a visit, etc.</p>
             <p>My purpose in using Google Analytics to collect this information is to improve my website and provide you with a better browsing experience. Rest assured, I respect your privacy and none of the information collected is personally identifiable.</p>
             <p>If you would like more information please visit
                 <a
@@ -35,21 +36,25 @@
 
             <h2>Complaints</h2>
             <p>If you have any complaints or if you believe your privacy has been breached, please <a href="mailto:peter.c.swift@gmail.com">email me</a> and provide details of the incident so it can be investigated.</p>
-            <p>If you wish to have the matter considered externally you can contact the Office of the Australian Information Commissioner
+            <p>If you wish to have the matter considered externally you can contact the
                 <a
                     href="https://www.oaic.gov.au/about-us/contact-us"
                     target="_blank"
                 >
-                    here
-                </a>.
+                    Office of the Australian Information Commissioner</a>.
             </p>
 
             <h2>Updates</h2>
             <p>Please note that this Privacy Policy may be updated from time to time and that by continuing to use thriveforlifechiropractic.com.au you are agreeing to these updates.</p>
 
-            <p>Last Update: 19 April, 2016</p>
+            <p class="last-update">Last Update: 19 April, 2016</p>
 
-            <p>Sincerely, Dr Peter Swift (Chiropractor and Owner of Thrive for Life Chiropractic)</p>
+            <p class="sincerely">Sincerely,
+                <br>
+                Dr Peter Swift
+                <br>
+                Chiropractor and Owner of Thrive for Life Chiropractic
+            </p>
         </div>
 
         <div slot="buttons">
