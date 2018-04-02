fdd3b3f50052249ecdbc7f9b5f36c9f3c11d4b45
Jenny Swift <cheezyspaghetti@gmail.com>
2018-04-02 19:02:40 +1000

    Comment out page description

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,7 +4,7 @@
 
 <head>
     <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-    <meta name="description" content="Quality chiropractic care tailored to your needs. Whether your goal is to relieve specific symptoms or to improve your overall health and long-term quality of life, I'm here to help.">
+    {{--<meta name="description" content="Quality chiropractic care tailored to your needs. Whether your goal is to relieve specific symptoms or to improve your overall health and long-term quality of life, I'm here to help.">--}}
     <title>Thrive for Life Chiropractic | Natural Health Care | Hurstville, Sydney | Dr Peter Swift (Chiropractor)</title>
     @include('templates.head-links')
 </head>
