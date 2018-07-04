68e9f103c2ae0b88c9d7492df0403a06fe5d48bd
Jenny Swift <cheezyspaghetti@gmail.com>
2018-07-04 12:39:42 +1000

    Try to fix deployment error

--- config/app.php
+++ config/app.php
@@ -176,7 +176,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Laracasts\Utilities\JavaScript\JavascriptServiceProvider::class
+        Laracasts\Utilities\JavaScript\JavaScriptServiceProvider::class
 
     ],
 
